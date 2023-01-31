038b463912df05c7305db88f49697d4357095d6d
mooongtto <119907935+mooongtto@users.noreply.github.com>
2023-01-31 20:40:36 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//공지영역 버티컬 넘어가는 제이쿼리
+// var swiper = new Swiper(".mySwiper", {
+//     direction: "vertical",
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
+
+// 배너 오른쪽 스와이퍼 넘어가는 제이쿼리
+// var swiper = new Swiper(".mySwiper", {
+//     pagination: {
+//       el: ".swiper-pagination",
+//     },
+//   });
